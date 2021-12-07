e4b32b9dd0940440cbcd003e5b199c9a134533f0
David Oblinsky <63413920+EldaveO@users.noreply.github.com>
2021-12-07 16:58:33 -0700

    Initial commit!

---
+++ Day1/DaveO.AdventOfCode.Day1/Measurements.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DaveO.AdventOfCode.Day1
+{
+    public class Measurements
+    {
+        public int Input;
+        public int Index;
+
+        public void Measurement(int input, int index)
+        {
+            Input = input;
+            Index = index;
+        }
+        
+        
+        private static List<Measurement> measurements = new List<Measurement>();
+
+        // variables used by the classs
+
+        // Initilize the class
+        
+        // methods
+        
+        // GetMeasurements and put them into a collection probably with a binary or text file? Clean the data maybe?
+        // 
+
+    }
+
+}
