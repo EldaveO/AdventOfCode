7101fc9e16b2c81905afc4e1bb0f3634ad76bd21
David Oblinsky <63413920+EldaveO@users.noreply.github.com>
2021-12-15 16:00:39 -0700

    boom baby

---
+++ Day2/AdventOfCode.Day2/FileAccessObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AdventOfCode.Day2
+{
+    public class FileAccessObject
+    {
+        public static void SaveText(string path, string value)
+        {
+            StreamWriter writer = new StreamWriter(path, true);
+            try
+            {
+                writer.WriteLine(value);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                writer.Close();
+            }
+
+
+        }
+
+        public static string[] GetText(string path)
+        {
+            //StreamReader reader = new StreamReader(path);
+            string[] contents = null;
+            try
+            {
+                contents = File.ReadAllLines(path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return contents;
+        }
+    }
+}
+
---
+++ Day2/AdventOfCode.Day2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var records = Position.GetPostions();
+
+            int forward = 0;
+            int depth = 0;
+
+            foreach (var position in records)
+            {
+                Console.WriteLine("{0}, {1}", position.position, position.value);
+
+                switch (position.position)
+                {
+                    case "forward":
+                        forward = position.value + forward;
+                        break;
+                    case "up":
+                        depth = depth - position.value;
+                        break;
+                    case "down":
+                        depth = depth + position.value;
+                        break;
+                }
+            }
+            Console.WriteLine("final forward position: {0}, final depth: {1}", forward, depth);
+            Console.WriteLine(forward * depth);
+            Console.ReadLine();
+        }
+    }
+}
