4988aa157fde0087a25ac37a151e3624dce472bf
David Oblinsky <63413920+EldaveO@users.noreply.github.com>
2021-12-21 10:50:39 -0700

    code got pretty
    I removed a bunch of lines and learned how to array good

--- Day3/DaveO.AdventOfCode.Day3/Program.cs
+++ Day3/DaveO.AdventOfCode.Day3/Program.cs
@@ -10,184 +10,23 @@ namespace DaveO.AdventOfCode.Day3
             int countOnes = 0;
             int countZeros = 0;
 
-            foreach (var rate in records)
+            for (int i = 0; i <= 11; i++)
             {
-                if (Rate.IsOne(rate.value1))
+                countOnes = 0;
+                countZeros = 0;
+                foreach (var rate in records)
                 {
-                    countOnes = countOnes + 1;
+                    if (Rate.IsOne(rate.values[i]))
+                    {
+                        countOnes = countOnes + 1;
+                    }
+                    else
+                    {
+                        countZeros = countZeros + 1;
+                    }
                 }
-                else
-                {
-                    countZeros = countZeros + 1;
-                }
+                Console.WriteLine("{2} bit: Ones:{0}, zeros:{1} ", countOnes, countZeros, i+1);
             }
-            Console.WriteLine("1 bit: Ones:{0}, zeros:{1} ", countOnes, countZeros);
-
-            countOnes = 0;
-            countZeros = 0;
-            foreach (var rate in records)
-            {
-                if (Rate.IsOne(rate.value2))
-                {
-                    countOnes = countOnes + 1;
-                }
-                else
-                {
-                    countZeros = countZeros + 1;
-                }
-            }
-            Console.WriteLine("2 bit: Ones:{0}, zeros:{1} ", countOnes, countZeros);
-
-            countOnes = 0;
-            countZeros = 0;
-            foreach (var rate in records)
-            {
-                if (Rate.IsOne(rate.value3))
-                {
-                    countOnes = countOnes + 1;
-                }
-                else
-                {
-                    countZeros = countZeros + 1;
-                }
-            }
-            Console.WriteLine("3 bit: Ones:{0}, zeros:{1} ", countOnes, countZeros);
-
-            countOnes = 0;
-            countZeros = 0;
-            foreach (var rate in records)
-            {
-                if (Rate.IsOne(rate.value4))
-                {
-                    countOnes = countOnes + 1;
-                }
-                else
-                {
-                    countZeros = countZeros + 1;
-                }
-            }
-            Console.WriteLine("4 bit: Ones:{0}, zeros:{1} ", countOnes, countZeros);
-
-            countOnes = 0;
-            countZeros = 0;
-            foreach (var rate in records)
-            {
-                if (Rate.IsOne(rate.value5))
-                {
-                    countOnes = countOnes + 1;
-                }
-                else
-                {
-                    countZeros = countZeros + 1;
-                }
-            }
-            Console.WriteLine("5 bit: Ones:{0}, zeros:{1} ", countOnes, countZeros);
-
-            countOnes = 0;
-            countZeros = 0;
-            foreach (var rate in records)
-            {
-                if (Rate.IsOne(rate.value6))
-                {
-                    countOnes = countOnes + 1;
-                }
-                else
-                {
-                    countZeros = countZeros + 1;
-                }
-            }
-            Console.WriteLine("6 bit: Ones:{0}, zeros:{1} ", countOnes, countZeros);
-
-            countOnes = 0;
-            countZeros = 0;
-            foreach (var rate in records)
-            {
-                if (Rate.IsOne(rate.value7))
-                {
-                    countOnes = countOnes + 1;
-                }
-                else
-                {
-                    countZeros = countZeros + 1;
-                }
-            }
-            Console.WriteLine("7 bit: Ones:{0}, zeros:{1} ", countOnes, countZeros);
-
-            countOnes = 0;
-            countZeros = 0;
-            foreach (var rate in records)
-            {
-                if (Rate.IsOne(rate.value8))
-                {
-                    countOnes = countOnes + 1;
-                }
-                else
-                {
-                    countZeros = countZeros + 1;
-                }
-            }
-            Console.WriteLine("8 bit: Ones:{0}, zeros:{1} ", countOnes, countZeros);
-
-            countOnes = 0;
-            countZeros = 0;
-            foreach (var rate in records)
-            {
-                if (Rate.IsOne(rate.value9))
-                {
-                    countOnes = countOnes + 1;
-                }
-                else
-                {
-                    countZeros = countZeros + 1;
-                }
-            }
-            Console.WriteLine("9 bit: Ones:{0}, zeros:{1} ", countOnes, countZeros);
-
-            countOnes = 0;
-            countZeros = 0;
-            foreach (var rate in records)
-            {
-                if (Rate.IsOne(rate.value10))
-                {
-                    countOnes = countOnes + 1;
-                }
-                else
-                {
-                    countZeros = countZeros + 1;
-                }
-            }
-            Console.WriteLine("10 bit: Ones:{0}, zeros:{1} ", countOnes, countZeros);
-
-            countOnes = 0;
-            countZeros = 0;
-            foreach (var rate in records)
-            {
-                if (Rate.IsOne(rate.value11))
-                {
-                    countOnes = countOnes + 1;
-                }
-                else
-                {
-                    countZeros = countZeros + 1;
-                }
-            }
-            Console.WriteLine("11 bit: Ones:{0}, zeros:{1} ", countOnes, countZeros);
-
-            countOnes = 0;
-            countZeros = 0;
-            foreach (var rate in records)
-            {
-                if (Rate.IsOne(rate.value12))
-                {
-                    countOnes = countOnes + 1;
-                }
-                else
-                {
-                    countZeros = countZeros + 1;
-                }
-            }
-            Console.WriteLine("12 bit: Ones:{0}, zeros:{1} ", countOnes, countZeros);
-
             Console.ReadLine();
         }
     }
--- Day3/DaveO.AdventOfCode.Day3/Rate.cs
+++ Day3/DaveO.AdventOfCode.Day3/Rate.cs
@@ -11,35 +11,14 @@ namespace DaveO.AdventOfCode.Day3
         //fields
         const string path = @"C:\Users\davido\Documents\DaveCode\AdventOfCode\Day3\Data.csv";
 
-        public int value1;
-        public int value2;
-        public int value3;
-        public int value4;
-        public int value5;
-        public int value6;
-        public int value7;
-        public int value8;
-        public int value9;
-        public int value10;
-        public int value11;
-        public int value12;
+        public int[] values;
 
         //constructors
         public Rate() { }
-        public Rate(int value1, int value2, int value3, int value4, int value5, int value6, int value7, int value8, int value9, int value10, int value11, int value12)
+
+        public Rate(int[] values)
         {
-            this.value1 = value1;
-            this.value2 = value2;
-            this.value3 = value3;
-            this.value4 = value4;
-            this.value5 = value5;
-            this.value6 = value6;
-            this.value7 = value7;
-            this.value8 = value8;
-            this.value9 = value9;
-            this.value10 = value10;
-            this.value11 = value11;
-            this.value12 = value12;
+            this.values = values;
         }
 
         //properties
@@ -55,9 +34,7 @@ namespace DaveO.AdventOfCode.Day3
             foreach (string record in records)
             {
                 rate = record.Split(char.Parse(","));
-                rat = new Rate(int.Parse(rate[0]), int.Parse(rate[1]), int.Parse(rate[2]), int.Parse(rate[3]),
-                                int.Parse(rate[4]), int.Parse(rate[5]), int.Parse(rate[6]), int.Parse(rate[7]),
-                                int.Parse(rate[8]), int.Parse(rate[9]), int.Parse(rate[10]), int.Parse(rate[11]));
+                rat = new Rate(Array.ConvertAll(rate, int.Parse));
                 rates.Add(rat);
             }
             return rates;
@@ -74,7 +51,5 @@ namespace DaveO.AdventOfCode.Day3
                 return false;
             }
         }
-
-
     }
 }
