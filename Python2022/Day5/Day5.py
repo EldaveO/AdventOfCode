7f9c6e0713b31c4fdcd1db2d5f52f456b07d7c54
BriMerhi <david.oblinsky@gmail.com>
2022-12-08 21:08:25 -0700

    day5 done

--- Python2022/Day5/Day5.py
+++ Python2022/Day5/Day5.py
@@ -1,7 +1,9 @@
 # input file
 #data = open("Day5\Sample.txt").readlines()
-moveList = open("Day5\SampleMoveList.txt").readlines()
-cargoStacks = open("Day5\SampleCargoStacks.txt").readlines()
+#moveList = open("Day5\SampleMoveList.txt").readlines()
+#cargoStacks = open("Day5\SampleCargoStacks.txt").readlines()
+moveList = open("Day5\MoveList.txt").readlines()
+cargoStacks = open("Day5\CargoStacks.txt").readlines()
 #data = open("Day5\Data.txt").readlines()
 
 def moveListFix(moveList):
@@ -19,23 +21,59 @@ def cargoStacksFix(cargoStacks):
 
 def part1(cargoStacks, moveList):
     i = int(0)
+    topStack = []
     for move in moveList:
         amountMove = int(moveList[i][0])
-        startMove = int(moveList[i][1])+1
+        startMove = int(moveList[i][1])-1
         endMove = int(moveList[i][2])-1
 
         j = int(0)
-        while j <= 1: #int(amountMove):
+        while j < amountMove:
             cargoCrate = cargoStacks[startMove][-1]
-            cargoStacks[startMove].pop([startMove][-1])
+            cargoStacks[startMove].pop()
             cargoStacks[endMove].append(cargoCrate)
-            print(cargoCrate)
-            print(cargoStacks)
-            j =+ 1
+            #print(cargoCrate)
+            #print(cargoStacks)
+            j += 1
         i += 1
 
-    return None
+    i = int(0)    
+    for stack in cargoStacks:
+        topStack.append(cargoStacks[i][-1])
+        i += 1
+    #print(topStack)    
+    return topStack
 
-part1(cargoStacksFix(cargoStacks), moveListFix(moveList))         
-#(part1(data))
-#print(part2(data))
+def part2(cargoStacks, moveList):
+    i = int(0)
+    topStack = []
+    cargoCrate = []
+    for move in moveList:
+        amountMove = int(moveList[i][0])
+        startMove = int(moveList[i][1])-1
+        endMove = int(moveList[i][2])-1
+
+        j = int(0)
+        while j < amountMove:
+            cargoCrate.append(cargoStacks[startMove][-1])
+            cargoStacks[startMove].pop()
+            
+            j += 1
+        cargoCrate.reverse()
+        k = int(0)
+        for cargo in cargoCrate:   
+            cargoStacks[endMove].append(cargoCrate[k])
+            k += 1
+        cargoCrate.clear() 
+        #print(cargoStacks)
+        i += 1
+
+    i = int(0)    
+    for stack in cargoStacks:
+        topStack.append(cargoStacks[i][-1])
+        i += 1
+    #print(topStack)    
+    return topStack    
+
+print(part1(cargoStacksFix(cargoStacks), moveListFix(moveList)))         
+print(part2(cargoStacksFix(cargoStacks), moveListFix(moveList)))    
