0488252f6540985223a65493eedaaee4b0a5e650
BriMerhi <david.oblinsky@gmail.com>
2022-12-08 22:57:28 -0700

    day7blank

---
+++ Python2022/Day7/Day7.py
@@ -0,0 +1,12 @@
+# input file
+data = open("Day6\Sample.txt").readline()
+#data = open("Day6\Data.txt").readline()
+
+def part1(data):
+    return None
+
+def part2(data):
+    return None    
+           
+print(part1(data))
+print(part2(data))
