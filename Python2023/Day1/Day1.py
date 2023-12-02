494d2f0f0c9feb058e4a481a6d1a997b7b80ddae
David Oblinsky <63413920+EldaveO@users.noreply.github.com>
2023-12-01 17:28:45 -0700

    day1 p1

--- Python2023/Day1/Day1.py
+++ Python2023/Day1/Day1.py
@@ -1,23 +1,29 @@
+import re
 # input file
 #data = open("Day1\Sample.txt").readlines()
 #data = open("Day1\Sample2.txt").readlines()
-data = open("Day1\Sample.txt").readlines()
+data = open("Data.txt").readlines()
 data = [line.strip() for line in data]
 #print(data)
 
 def part1(data):
-    firstDigit = int(11)
-    secondDigit = int(22)
-    Sums = list()
+    calibrationTotal = 0
     for line in data:
-           for char in line:
-                if firstDigit == 11 and char.isdigit(): 
-                    firstDigit = char
-                elif secondDigit == 22 and char.isdigit():
-                    secondDigit = char
-                elif firstDigit != 11 and secondDigit != 22:
-                    
+           firstDigit = next(char for char in line if char.isdigit())
+           lastDigit = next(char for char in reversed(line) if char.isdigit())
+           calibrationValue = int(firstDigit + lastDigit)
+           calibrationTotal += calibrationValue
+    return calibrationTotal
 
-        return
+def part2(data):
+    calibrationTotal = 0
+    for line in data:
+           digits = re.findall(r'\b(one|two|three|four|five|six|seven|eight|nine)\b|\d', line)
+           firstDigit = digits[0] if digits[0].isdigit() else str(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].index(digits[0]) + 1)
+           lastDigit = digits[-1] if digits[-1].isdigit() else str(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].index(digits[-1]) + 1)
+           calibrationValue = int(firstDigit + lastDigit)
+           calibrationTotal += calibrationValue
+    return calibrationTotal
 
 print(part1(data))
+print(part2(data))
