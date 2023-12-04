ab533e576436fed4191777abc5f9f73da969caf5
David Oblinsky <63413920+EldaveO@users.noreply.github.com>
2023-12-04 10:33:20 -0700

    Giving up on part 2
    Stupid regex and words being stuck together

--- Python2023/Day1/Day1.py
+++ Python2023/Day1/Day1.py
@@ -1,9 +1,7 @@
-import re
 # input file
-#data = open("Day1\Sample.txt").readlines()
-#data = open("Day1\Sample2.txt").readlines()
-data = open("Data.txt").readlines()
-data = [line.strip() for line in data]
+data = open("Sample.txt").readlines()
+#data = open("Data.txt").readlines()
+#data = [line.strip() for line in data]
 #print(data)
 
 def part1(data):
@@ -15,15 +13,12 @@ def part1(data):
            calibrationTotal += calibrationValue
     return calibrationTotal
 
-def part2(data):
-    calibrationTotal = 0
+def part2(data):    
     for line in data:
-           digits = re.findall(r'\b(one|two|three|four|five|six|seven|eight|nine)\b|\d', line)
-           firstDigit = digits[0] if digits[0].isdigit() else str(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].index(digits[0]) + 1)
-           lastDigit = digits[-1] if digits[-1].isdigit() else str(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].index(digits[-1]) + 1)
-           calibrationValue = int(firstDigit + lastDigit)
-           calibrationTotal += calibrationValue
-    return calibrationTotal
+        
+    return "done"
+      
 
-print(part1(data))
+#print(part1(data))
 print(part2(data))
+
